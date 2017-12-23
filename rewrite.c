656c76edc3bd6cf75d6f4dade396f165e2515007
squidcarroll <squidcarroll@gmail.com>
2017-12-23 02:16:10 +0000

    update files

--- rewrite.c
+++ rewrite.c
@@ -30,7 +30,7 @@ int main(int argc, char *argv[])
 		{NULL, NULL}
 	};
 
-	if (argc != 2 || access(argv[1], F_OK) == -1)
+	if (argc != 2)
 	{
 		printf("USAGE: monty file\n");
 		exit(EXIT_FAILURE);
